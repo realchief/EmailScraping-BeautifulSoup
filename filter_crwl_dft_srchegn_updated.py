668da92ad77ed436b87e2f4a4e2284dc0254a221
realchief <stryokka@gmail.com>
2019-07-25 16:00:31 -0400

    updated result

--- filter_crwl_dft_srchegn_updated.py
+++ filter_crwl_dft_srchegn_updated.py
@@ -33,57 +33,34 @@ def crawl(request_url):
         pass
     return emails
 
+
+def get_links(url):
+    link_result = []
+    request = urllib.request.Request(url)
+    response = urllib.request.urlopen(request)
+    html_page = response.read()
+    soup = BeautifulSoup(html_page, "lxml")
+    for link in soup.findAll('a'):
+        d = link.get('href')
+        link_result.append(d)
+    return link_result
+
    
 if __name__ == '__main__':
     
     domain = input("enter the domain:")
-    d = domain
-    url = 'https://duckduckgo.com/?q=email+"%40"+++'+d+'+++""&ia=web&count=50&first=51'
-    
-    requestd = urllib.request.Request(url)
-    responsed = urllib.request.urlopen(requestd)
-    html_paged = responsed.read()
-    soupd = BeautifulSoup(html_paged, "lxml")
-    for link in soupd.findAll('a'):
-        link_d = link.get('href')
-        d1 = link_d
-        link_3.append(d1)
 
-    y = domain
-    url = 'https://in.search.yahoo.com/search?p=%5B%40"%20+%20'+y+'%20+%20"%5D&pz=100'
-    
-    request_y = urllib.request.Request(url)
-    response_y = urllib.request.urlopen(request_y)
-    html_page_y = response_y.read()
-    soupy = BeautifulSoup(html_page_y, "lxml")
-    for link in soupy.findAll('a'):
-        link_y = link.get('href')
-        y1 = link_y
-        link_4.append(y1)
+    url_d = 'https://duckduckgo.com/?q=email+"%40"+++'+domain+'+++""&ia=web&count=50&first=51'
+    link_3 = get_links(url_d)
 
-    ya = domain
-    url = 'https://yandex.com/search/?text="%40"%20%20%20'+ya+'%20%20%20""&lr=20983'
-    
-    request_ya = urllib.request.Request(url)
-    response_ya = urllib.request.urlopen(request_ya)
-    html_page_ya = response_ya.read()
-    soup_ya = BeautifulSoup(html_page_ya, "lxml")
-    for link in soup_ya.findAll('a'):
-        link_ya = link.get('href')
-        ya1 = link_ya
-        link_5.append(ya1)
+    url_y = 'https://in.search.yahoo.com/search?p=%5B%40"%20+%20'+domain+'%20+%20"%5D&pz=100'
+    link_4 = get_links(url_y)
 
-    ask = domain
-    url = "https://www.ask.com/web?q=email+"+ask+"&o=0&qo=homepageSearchBox"
-    
-    request_ask = urllib.request.Request(url)
-    response_ask = urllib.request.urlopen(request_ask)
-    html_page_ask = response_ask.read()
-    soup_ask = BeautifulSoup(html_page_ask, "lxml")
-    for link in soup_ask.findAll('a'):
-        link_ask = link.get('href')
-        ask1 = link_ask
-        link_6.append(ask1)
+    url_ya = 'https://yandex.com/search/?text="%40"%20%20%20'+domain+'%20%20%20""&lr=20983'
+    link_5 = get_links(url_ya)
+
+    url_ask = "https://www.ask.com/web?q=email+"+domain+"&o=0&qo=homepageSearchBox"
+    link_6 = get_links(url_ask)
 
     links = link_3 + link_4 + link_5 + link_6
     nodup_link = list(set(links))

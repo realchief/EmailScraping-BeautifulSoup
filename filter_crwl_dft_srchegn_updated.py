8b05befd244d1d26785f48365d2c98e561e8bcc8
realchief <stryokka@gmail.com>
2019-07-25 12:07:25 -0400

    added writing json file

--- filter_crwl_dft_srchegn_updated.py
+++ filter_crwl_dft_srchegn_updated.py
@@ -1,9 +1,9 @@
 import requests
-import re
 import time
 from bs4 import BeautifulSoup
 import urllib.request
 import re
+import json
 start_time = time.time()
 link_3 = []
 link_4 = []
@@ -17,17 +17,16 @@ d = ""
 y = ""
 ya = ""
 ask = ""
-
+domain = ""
 emails = []
 new_emails = []
 mails = []
 
 
-def crawl(x):
+def crawl(request_url):
     try:
-        response = requests.get(x)
-        # new_emails = re.findall(r"[a-z0-9\.\-+_]+@[a-z0-9\.\-+_]+\.[a-z]+", response.text)
-        new_emails = re.findall(r"[a-z0-9\.\-+_]+@"+q, response.text)
+        response = requests.get(request_url)
+        new_emails = re.findall(r"[a-z0-9\.\-+_]+@" + domain, response.text)
         if new_emails:
             emails.append(new_emails)
     except:
@@ -37,8 +36,8 @@ def crawl(x):
    
 if __name__ == '__main__':
     
-    q = input("enter the domain:")
-    d = q    
+    domain = input("enter the domain:")
+    d = domain
     url = 'https://duckduckgo.com/?q=email+"%40"+++'+d+'+++""&ia=web&count=50&first=51'
     
     requestd = urllib.request.Request(url)
@@ -50,37 +49,37 @@ if __name__ == '__main__':
         d1 = link_d
         link_3.append(d1)
 
-    y = q  
+    y = domain
     url = 'https://in.search.yahoo.com/search?p=%5B%40"%20+%20'+y+'%20+%20"%5D&pz=100'
     
     request_y = urllib.request.Request(url)
     response_y = urllib.request.urlopen(request_y)
     html_page_y = response_y.read()
-    soupy = BeautifulSoup(html_page_y,"lxml")
+    soupy = BeautifulSoup(html_page_y, "lxml")
     for link in soupy.findAll('a'):
         link_y = link.get('href')
         y1 = link_y
         link_4.append(y1)
 
-    ya = q
+    ya = domain
     url = 'https://yandex.com/search/?text="%40"%20%20%20'+ya+'%20%20%20""&lr=20983'
     
     request_ya = urllib.request.Request(url)
     response_ya = urllib.request.urlopen(request_ya)
     html_page_ya = response_ya.read()
-    soup_ya = BeautifulSoup(html_page_ya,"lxml")
+    soup_ya = BeautifulSoup(html_page_ya, "lxml")
     for link in soup_ya.findAll('a'):
         link_ya = link.get('href')
         ya1 = link_ya
         link_5.append(ya1)
 
-    ask = q
+    ask = domain
     url = "https://www.ask.com/web?q=email+"+ask+"&o=0&qo=homepageSearchBox"
     
     request_ask = urllib.request.Request(url)
     response_ask = urllib.request.urlopen(request_ask)
     html_page_ask = response_ask.read()
-    soup_ask = BeautifulSoup(html_page_ask,"lxml")
+    soup_ask = BeautifulSoup(html_page_ask, "lxml")
     for link in soup_ask.findAll('a'):
         link_ask = link.get('href')
         ask1 = link_ask
@@ -90,25 +89,22 @@ if __name__ == '__main__':
     nodup_link = list(set(links))
     print(nodup_link)
     string = "http"
+    filtered_links = [i for i in nodup_link if re.search(string, i)]
+    final_links = [j for j in filtered_links]
+    mails = [crawl(f) for f in final_links]
+    print(mails)
 
-#    for i in links:
-#        if re.search(string , i):
-#            filtered_links.append(i)
-    filtered_links = [i for i in nodup_link if re.search(string , i)]
-    linkss = [j for j in filtered_links]
-#    for f in linkss:
-#        mails.append(crawl(f))
-
-    mails = [crawl(f) for f in linkss]
-    result_copy = mails
-print("-------------ol")
-print(mails)
-print("-------------")
-
-flat = [j for i in emails for j in i]
-seen =[]
+flat = [j for i in mails for j in i]
+seen = []
 for item in flat:
-   if item not in seen:
-      seen.append(item)
+    if item not in seen:
+        seen.append(item)
 print(seen)
-print("--- %s seconds ---" % (time.time() - start_time))      
+data = {}
+data.update({
+    'domain': domain,
+    'emails': seen
+})
+with open('data.json', 'w') as outfile:
+    json.dump(data, outfile)
+print("--- %s seconds ---" % (time.time() - start_time))
